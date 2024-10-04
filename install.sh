7838579584b3256d44d24f30a2b755885d509f09
angelsen <fredrikangelsen@gmail.com>
2024-10-04 15:27:16 +0200

    Use apt to install luma.oled instead of pip
    - Updated install.sh to use `python3-luma.oled` from Debian package repository
    - Removed pip installation of luma.oled to avoid conflicts with Debian's managed Python environment (PEP 668)
    - Simplified installation by leveraging system package management for better stability and compatibility

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 INSTALL_PATH="/usr/local/"
 SYSTEMD_PATH="/etc/systemd/"
-DEPS="python3-dev python3-pip python3-setuptools python3-wheel python3-psutil libfreetype6-dev libjpeg-dev build-essential"
+DEPS="python3-dev python3-psutil libfreetype6-dev libjpeg-dev build-essential python3-luma.oled"
 
 if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root!"
@@ -13,9 +13,6 @@ fi
 echo "Installing Dependencies"
 apt-get update
 apt-get install -y $DEPS
-
-echo "Installing luma.oled library"
-pip3 install --upgrade luma.oled
 
 echo "Installing sys-oled files"
 cp -fv etc/sys-oled.conf  /etc
